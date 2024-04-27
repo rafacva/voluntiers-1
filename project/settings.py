ea5a4d8acfcbae9fed75d7994d39a3188c4cb515
PedroR-19 <p.scianni@aluno.ifsp.edu.br>
2024-04-27 13:01:39 -0300

    --chat

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'vagas',
     'profiles',
-    'chat',
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('vagas.urls')),
     path('profiles/', include('profiles.urls')),
-    path('chat/', include('chat.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
